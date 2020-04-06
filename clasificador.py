d48a25050a327d15eadb9c8f100a630f3af99ad8
Miguel101297 <43767776+Miguel101297@users.noreply.github.com>
2020-04-06 12:59:13 -0600

    Delete clasificador.py

--- clasificador.py
+++
@@ -1,5 +0,0 @@
-import csv
-from textblob.classifiers import NaiveBayesClassifier
-with open('categoria.json','r') as fp:
-  cl=NaiveBayesClassifier(fp,format="json")
-  print(cl.classify("Propone AMLO a Belinda para la Secretaría de Cultura ¡YA LE TIRÓ SU HUESO!"))
